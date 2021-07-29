7518a3558841139c1c17df3b9ede97c1658bf302
Your Name <nickmihaly@gmail.com>
2021-07-28 19:29:48 -0700

    HEATMAP state and date search functional!

--- heatmap/heatmap.js
+++ heatmap/heatmap.js
@@ -1,24 +1,37 @@
 //const axios = ('axios').default;
 // axios.<method> will now provide autocomplete and parameter typings
+var heatmapData = [];
 
-axios.get('http://localhost:3001/api/sightings/state/CA')
+axios.get('http://localhost:3001/api/sightings/search?state=CA&date1=04/01/2021&date2=05/31/2021')
 .then (function (res) {
+  //then((resp) => resp.json());
+  // Log full response
   console.log(res);
+
+  //Log lat/long
+for (let i = 0; i< res.data.length; i++)
+{
+  heatmapData.push(new google.maps.LatLng(res.data[i].latitude, res.data[i].longitude));
+}
+})
+.then(() => {
+ initMap();
 })
 .catch(function (error) {
   console.log(error);
-})
-.then(function () {
-  // always executed
-});  
+});
+
 
 function initMap(){
-/* Data points defined as an array of LatLng objects */
-var heatmapData = [
-  new google.maps.LatLng(37.782, -122.447),
-  new google.maps.LatLng(37.782, -122.445),
-  new google.maps.LatLng(37.782, -122.443),
-  new google.maps.LatLng(37.782, -122.441),
+
+ 
+ 
+ //Data points defined as an array of LatLng objects 
+/*var heatmapData = [
+  {location:new google.maps.LatLng(33.384, -86.411), weight: 1000},
+  new google.maps.LatLng(30.695, -88.039),
+  new google.maps.LatLng(30.246, -87.700),
+  {location: new google.maps.LatLng(34.026, -86.326), weight: 1000},
   new google.maps.LatLng(37.782, -122.439),
   new google.maps.LatLng(37.782, -122.437),
   new google.maps.LatLng(37.782, -122.435),
@@ -28,15 +41,16 @@ var heatmapData = [
   new google.maps.LatLng(37.785, -122.441),
   new google.maps.LatLng(37.785, -122.439),
   new google.maps.LatLng(37.785, -122.437),
-  new google.maps.LatLng(37.785, -122.435)
-];
+  new google.maps.LatLng(37.785, -122.435),
+  {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+];*/
 
-var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+var AL = new google.maps.LatLng(34.0262082, -86.3260923);
 
 map = new google.maps.Map(document.getElementById('map'), {
-  center: sanFrancisco,
+  center: AL,
   zoom: 13,
-  mapTypeId: 'satellite'
+  map: map
 });
 
 var heatmap = new google.maps.visualization.HeatmapLayer({
